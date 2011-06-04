7a7be2b50c833462fcb71d8edebea75bf8d5c4e8
Daniel Sztutwojner <sztuto@gmail.com>
2011-06-04 13:33:40 +0300

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "D S"
+  user.email                  "ds@ex.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
