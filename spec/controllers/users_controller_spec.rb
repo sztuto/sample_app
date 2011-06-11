de3b7ef2ade42171efba4c4d44133bea04b00fae
Daniel Sztutwojner <sztuto@gmail.com>
2011-06-11 14:34:21 +0300

    Users index with pagination

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -20,6 +20,11 @@ describe UsersController do
         @user = test_sign_in(Factory(:user))
         Factory(:user, :email => "another@example.com")
         Factory(:user, :email => "another@example.net")
+        
+        30.times do
+          Factory(:user, :email => Factory.next(:email))
+        end
+          
       end
       
       it "should be successful" do
@@ -37,6 +42,14 @@ describe UsersController do
         User.all.each do |user|
           response.should have_selector('li', :content => user.name)
         end
+      end
+      
+      it "should paginate users" do
+        get :index
+        response.should have_selector('div.pagination')
+        response.should have_selector('span.disabled', :content => "Previous")
+        response.should have_selector('a', :href => "/users?page=2", :content => "2")
+        response.should have_selector('a', :href => "/users?page=2", :content => "Next")
       end
       
     end
